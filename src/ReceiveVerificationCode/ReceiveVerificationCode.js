aa4d2cc382286b145c26ad68af81135436124e85
Lara Elisabeth Fredrich <lfre@itu.dk>
2022-11-16 18:31:34 +0100

    implement PopOver Verification code

--- src/ReceiveVerificationCode/ReceiveVerificationCode.js
+++ src/ReceiveVerificationCode/ReceiveVerificationCode.js
@@ -5,6 +5,7 @@ import './ReceiveVerificationCode.css';
 import { Button } from 'react-native-web';
 import { Context } from "../Context";
 import { useContext} from 'react';
+import PopOverVerificationCode from './PopOverVerificationCode';
 
 export default function ReceiveVerificationCode() {
    
@@ -31,31 +32,30 @@ export default function ReceiveVerificationCode() {
 
   return (
     <div className='container'>
-    <div className='content'>
-        <div className='text-area'>
-            <h1>Velkommen</h1>
-            <div className='intro-text '>
+        <div className='content'>
+            <div className='text-area'>
+                <h1>Velkommen</h1>
+                <div className='space-between'>
 
-            <p>Nedenunder ser du din unikke verifikationskode, som du skal bruge senere til at tjekke, at din stemme er optalt korrekt.</p>
-            <p>Gem den et sted, hvor du kan finde den.</p>
+                <p>Nedenunder ser du din unikke verifikationskode, som du skal bruge senere til at tjekke, at din stemme er optalt korrekt.</p>
+                <p>Gem den et sted, hvor du kan finde den.</p>
+                </div>
+
+          
+
+            <div className='read-more'>
+                {/* Make a pop-up */}
+                <a className='link-bold' href="">Læs mere</a>
             </div>
 
-        </div>
-
-        <div className='read-more'>
-            {/* Make a pop-up */}
-            <a className='link-bold' href="">Læs mere</a>
-        </div>
-
-        <div id='container'>
-        <div className='verification-code'>
+            <div className='space-between'>
+            <div className='verification-code'>
             <h3>{context.inputCode}</h3>
         </div>
-            <a className='button-verification-code' onClick={()=> navigateToVotingPage()}>
-                Stem nu
-            </a>
-        </div>    
-        </div>   
-</div>
+            <PopOverVerificationCode/>
+            </div>    
+            </div>  
+            </div> 
+    </div>
   )
 }

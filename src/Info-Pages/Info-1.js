a58b20ba82d061aedc14cc1b28ba0f2d75e2dc7c
Lara Elisabeth Fredrich <lfre@itu.dk>
2023-03-20 12:21:47 +0100

    layout

--- src/Info-Pages/Info-1.js
+++ src/Info-Pages/Info-1.js
@@ -100,9 +100,11 @@ export default function Info1() {
                     <Text>1</Text>
                   </GridItem>
                   <GridItem className="info1-steps-griditem">
+                    <Text>
                     All candidates are fictional and for the purpose of this
                     study we ask you to vote for{" "}
                     <span className="bold-text red-text">Sarah Wilson.</span>
+                    </Text>
                     <Checkbox
                       className="check-box check-box-red"
                       isChecked={checked}

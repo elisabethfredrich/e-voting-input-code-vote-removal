a470d6c07558e8e8c9bc1ed94586051bdc383fbc
Lara Elisabeth Fredrich <lfre@itu.dk>
2023-04-15 13:30:20 +0200

    fix redirection to system 3 when 30 voters are registered

--- src/App.js
+++ src/App.js
@@ -26,12 +26,16 @@ Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_KEY);
 Parse.serverURL = PARSE_HOST_URL;
 
 function App() {
-  
+
   useEffect(()=>{
-    if(getNumberOfVoters()===30){
-      window.location.href = "https://e-voting-study-3.netlify.app/info-1";
-    }
-  }, [])
+    let count;
+    getNumberOfVoters().then((res) => {
+      count = res;
+      if(count>=30){
+         window.location.href = "https://e-voting-study-3.netlify.app/info-1";
+       }
+    });
+  },[])
 
   return (
     <div className="App">
